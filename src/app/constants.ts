1e66364e57a227d293ea8a4be77d38b300525cb0
cryptods8 <dusoo.s+ds8@gmail.com>
2026-04-21 08:50:54 +0200

    remove airstack

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -4,15 +4,7 @@ const fallbackHubHttpUrl = isProduction
   ? undefined
   : "http://localhost:3010/hub";
 
-const airstackApiKey = process.env.AIRSTACK_API_KEY;
-
-export const hubHttpUrl = airstackApiKey
-  ? "https://hubs.airstack.xyz"
-  : process.env.HUB_HTTP_URL || fallbackHubHttpUrl;
-export const hubRequestOptions = airstackApiKey
-  ? {
-      headers: { "x-airstack-hubs": airstackApiKey },
-    }
-  : undefined;
+export const hubHttpUrl = "https://hub.pinata.cloud";
+export const hubRequestOptions = undefined;
 
 export const externalBaseUrl = process.env.NEXT_PUBLIC_URL || "http://localhost:3000";
