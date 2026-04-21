dbc674252c92b6f678b0ddb91f638025b7edbfc0
cryptods8 <dusoo.s+ds8@gmail.com>
2026-04-21 09:22:05 +0200

    fix

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,9 +1,5 @@
 export const isProduction = process.env.VERCEL_ENV === "production";
 
-const fallbackHubHttpUrl = isProduction
-  ? undefined
-  : "http://localhost:3010/hub";
-
 export const hubHttpUrl = "https://hub.pinata.cloud";
 export const hubRequestOptions = undefined;
 
